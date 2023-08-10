25df605fdd74baadf0ba93cc259ce1b6adbd9a12
Adrian333Dev <adrian333dev@gmail.com>
2023-08-10 17:31:49 +0400

    bootstap setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BootstrapModule } from './shared/modules/bootstrap.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BootstrapModule],
 })
 export class AppComponent {}
---
+++ src/app/shared/modules/bootstrap.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const modules = [NgbModule];
+
+@NgModule({
+  imports: [...modules],
+  exports: [...modules],
+})
+export class BootstrapModule {}
