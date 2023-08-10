cec14315bf07a2cb995f1a80a62e621980c694d5
Adrian333Dev <adrian333dev@gmail.com>
2023-08-11 02:21:41 +0400

    save changes

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BootstrapModule } from 'src/app/shared/modules/bootstrap.module';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BootstrapModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  form: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+}
